7a4921a26f98562e78bada1f085f73190d88a140
LuisM11 <lumarinb@unal.edu.co>
2021-11-09 23:05:40 -0500

    m

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,23 +6,38 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+    def __str__(self):
+        return "Grupo de estudiantes: "+ self._grupo
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
-
-    def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+    def listadoAsignaturas(self, **kwargs):
+        if self._asignaturas is not None:
+            for x in kwargs:
+                self._asignaturas.append(kwargs[x])
         else:
-            self.listadoAlumnos = [alumno]
+            self._asignaturas=[]
+            for x in kwargs:
+                self._asignaturas.append(kwargs[x])
+
+
+    def agregarAlumno(self, alumno, lista=None):
+        if(lista is None):
+            if self.listadoAlumnos is None:
+                self.listadoAlumnos= [alumno]
+            else:
+                self.listadoAlumnos = self.listadoAlumnos + [alumno]
+        else:
+            if self.listadoAlumnos is None:
+                self.listadoAlumnos = lista
+                self.listadoAlumnos.append(alumno)
+            else:
+                self.listadoAlumnos = self.listadoAlumnos + lista
+                self.listadoAlumnos.append(alumno)
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -32,6 +47,4 @@ class Grupo:
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    
